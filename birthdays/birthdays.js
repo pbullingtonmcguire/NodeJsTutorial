8b6354f2bca755ee21ba41151b6b8aeb5bf3ae52
Parker Bullington-McGuire <pbullingtonmcguire@gmail.com>
2022-04-01 22:50:10 -0400

    Stored the birthday list in a json file instead of hardcoding it

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,41 +1,46 @@
 birthdaysJson = getJson("birthdays.json");
 birthdaysJson.then(
-  function(value) {console.log("success:" + value.toString());},
+  function(value) {console.log("successfully loaded json");},
   function(error) {console.log(error);}
 );
-updateDate(new Date());
+updateDate(new Date(), birthdaysJson);
 
 async function getJson(fileName) {
   const response = await fetch(fileName);
   const json = await response.json();
   console.log(json);
-  //json = {Parker: "05/30/2001"};
   return json;
 }
 
 function updateDate(date) {
-  document.getElementById("todaysDate").innerHTML = "Today is " + date.toDateString() + ".";
-  const birthdays = {Parker: new Date("05/30/2001"), Bob: new Date ("03/17/2000"), Alice: new Date("03/17/1998"),
-                     Shoshana: new Date("04/12/2007"), Declan: new Date("04/22/2009"), Susie: new Date("03/17/2002")};
-  const birthdaysToday = [];
-  for (person in birthdays) {
-    if ( date.getMonth() == birthdays[person].getMonth() & date.getDate() == birthdays[person].getDate() ) {
-        birthdaysToday.push(person);
-    }
-  }
-  var birthdayMessage = "";
-  if (birthdaysToday.length) {
-    birthdayMessage = "Happy birthday to ";
-    for (let i = 0; i < birthdaysToday.length; i++) {
-      birthdayMessage += birthdaysToday[i];
-      switch (birthdaysToday.length - 1 - i){
-        case 0: birthdayMessage += "!"; break;
-        case 1: birthdayMessage += (birthdaysToday.length >= 3) ? ", and " : " and "; break;
-        default: birthdayMessage += ", ";
+  birthdaysJson.then(
+    function(value) {
+      document.getElementById("todaysDate").innerHTML = "Today is " + date.toDateString() + ".";
+      const birthdays = value;
+      const birthdaysToday = [];
+      const dateString = date.toDateString().substring(4, 10);
+      console.log(dateString);
+      for (person in birthdays) {
+        if ( dateString == birthdays[person] ) {
+            birthdaysToday.push(person);
+        }
       }
-    }
-  }
-  document.getElementById("birthdayMessage").innerHTML = birthdayMessage;
+      var birthdayMessage = "";
+      if (birthdaysToday.length) {
+        birthdayMessage = "Happy birthday to ";
+        for (let i = 0; i < birthdaysToday.length; i++) {
+          birthdayMessage += birthdaysToday[i];
+          switch (birthdaysToday.length - 1 - i){
+            case 0: birthdayMessage += "!"; break;
+            case 1: birthdayMessage += (birthdaysToday.length >= 3) ? ", and " : " and "; break;
+            default: birthdayMessage += ", ";
+          }
+        }
+      }
+      document.getElementById("birthdayMessage").innerHTML = birthdayMessage;
+    },
+    function(error) {console.log(error);}
+  )
 }
 function changeDate() {
   let date = new Date( prompt( "Enter a date", new Date().toDateString().substring(4) ) );
